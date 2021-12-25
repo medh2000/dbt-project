a18f0e3c0fcb92ab6e337817e7a119bc9dfd39f1
hassanmoh@microsoft.com <hassanmoh@microsoft.com>
2021-12-25 09:11:37 +0000

    files updated

--- models/marts/core/fct_orders.sql
+++ models/marts/core/fct_orders.sql
@@ -1,13 +1,27 @@
-with fct_orders as (
+with orders as  (
+    select * from {{ ref('stg_orders' )}}
+),
 
+payments as (
+    select * from {{ ref('stg_payments') }}
+),
+
+order_payments as (
+    select order_id,
+    sum(case when STATUS = 'success' then amount end) as amount
+    from payments
+    group by 1
+),
+
+final as (
     select
-        order_id,
-        customer_id,
-        amount
-
-    from {{ ref('stg_orders')}}
-    left outer join {{ ref('stg_payments') }}
+    orders.order_id,
+    orders.customer_id,
+    orders.order_date,
+    coalesce(order_payments.amount, 0) as amount
+    from orders
+    left outer join order_payments
     using (order_id)
 
 )
-select * from fct_orders
+select * from final
--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,11 +1,14 @@
 with stg_payments as (
 
     select
+        id as payment_id,
         orderid as order_id,
-        created,
-        paymentmethod,
-        amount
-    from raw.stripe.payment
+        status,
+        paymentmethod as payment_method,
+        -- amount is stored in cents, convert it to dollars
+        amount / 100 as amount,
+        created as created_at
+    from {{ source('stripe', 'payment')}} --raw.stripe.payment
 
 )
 select * from stg_payments
